8943e281dbc8624c8ee458a02833986e8d3b3ff0
Vagner Torres <esdras.torres.vt@gmail.com>
2017-04-19 09:52:44 -0300

    adicionando novo exercicio

---
+++ src/br/com/exercicio/seq/PesoIdeal.java
@@ -0,0 +1,19 @@
+package br.com.exercicio.seq;
+
+import java.util.Scanner;
+
+public class PesoIdeal {
+
+	public static void main(String[] args) {
+
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.println("Digite sua altura");
+		double altura = scanner.nextDouble();
+
+		double pesoIdeal = (72.7 * altura) - 58;
+
+		System.out.println("o peso ideal para a altura informada é: " + pesoIdeal);
+	}
+
+}
