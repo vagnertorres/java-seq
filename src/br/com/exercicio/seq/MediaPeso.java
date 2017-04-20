27624ebf739323928e2ce9b49f186fa9f66a9a2b
Vagner Torres <esdras.torres.vt@gmail.com>
2017-04-20 09:01:29 -0300

    novos exercícios resolvidos

---
+++ src/br/com/exercicio/seq/MediaPeso.java
@@ -0,0 +1,34 @@
+package br.com.exercicio.seq;
+
+import java.util.Scanner;
+
+public class MediaPeso {
+
+	public static void main(String[] args) {
+
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.println("Digite a sua altura");
+		double altura = scanner.nextDouble();
+
+		System.out.println("Digite seu sexo");
+		String sexo = scanner.next();
+
+		if (sexo.equalsIgnoreCase("homem")) {
+
+			double mediaPesoHomem = ((72.7 * altura) - 58);
+
+			System.out.println("Peso ideal é: " + mediaPesoHomem);
+
+		} else if (sexo.equalsIgnoreCase("mulher")) {
+
+			double mediaPesoMulher = ((62.1 * altura) - 44.7);
+
+			System.out.println("Peso ideal é: " + mediaPesoMulher);
+
+		} else {
+
+			System.out.println("sexo inválido");
+		}
+	}
+}
